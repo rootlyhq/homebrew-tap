a0d148a500dc7b4848f3471338fa6d08c5fdbb2c
Alexander Mingoia <alex@alexmingoia.com>
2024-10-18 13:47:09 -0700

    Terraformer 2.7.1

--- Formula/terraformer-rootly.rb
+++ Formula/terraformer-rootly.rb
@@ -1,20 +1,20 @@
 class TerraformerRootly < Formula
   desc "Generate Terraform configuration from Rootly"
   homepage "https://rootly.com"
-  version "2.1.0"
+  version "2.7.1"
 
   on_macos do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/rootlyhq/terraformer/releases/download/v2.1.0/terraformer-rootly-darwin-arm64"
-      sha256 "be43e82e0d8e4fbd3329ce62c2a810b69a2efb4c0c3f43689703ef6fed35aca8"
+      url "https://github.com/rootlyhq/terraformer/releases/download/v2.7.1/terraformer-rootly-darwin-arm64"
+      sha256 "f9aaad99ad6b472557ea76bd9a36d49ff1ed96984ac74173a91cd9611e55ce8b"
 
       def install
         bin.install "terraformer-rootly-darwin-arm64" => "terraformer-rootly"
       end
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/rootlyhq/terraformer/releases/download/v2.1.0/terraformer-rootly-darwin-amd64"
-      sha256 "c269cddea01c647f5bd9cccae5653e1326385198c82765410edee5de9aeb3b7e"
+      url "https://github.com/rootlyhq/terraformer/releases/download/v2.7.1/terraformer-rootly-darwin-amd64"
+      sha256 "bad5424e6795dc60786abf1cb1f4c31b5f2f56965a7fe8355763d345054600e2"
 
       def install
         bin.install "terraformer-rootly-darwin-amd64" => "terraformer-rootly"
@@ -24,16 +24,16 @@ class TerraformerRootly < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/rootlyhq/terraformer/releases/download/v2.1.0/terraformer-rootly-linux-arm64"
-      sha256 "8d7c8232e282e4f5e9f614412c2266e4a4c2d4ace22a54e316f934e7d55a0815"
+      url "https://github.com/rootlyhq/terraformer/releases/download/v2.7.1/terraformer-rootly-linux-arm64"
+      sha256 "07765df16d8f043bd8eafaf47b7884154ceeb97387c74d345021e06506a29fa6"
 
       def install
         bin.install "terraformer-rootly-linux-arm64" => "terraformer-rootly"
       end
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/rootlyhq/terraformer/releases/download/v2.1.0/terraformer-rootly-linux-amd64"
-      sha256 "89e83d95f2fa019bbd40e6394c8530f75239f3c783c799ac2b99232a7ec0aa56"
+      url "https://github.com/rootlyhq/terraformer/releases/download/v2.7.1/terraformer-rootly-linux-amd64"
+      sha256 "dcaee05e46a858fb2cf65cde689d55cebcb0f87abbcebdf7cdd1f40961e054fe"
 
       def install
         bin.install "terraformer-rootly-linux-amd64" => "terraformer-rootly"
