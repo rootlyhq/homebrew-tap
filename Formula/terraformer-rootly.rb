86cc19049c7ee8835e3a159e3c649aa81383c430
Alexander Mingoia <alex@alexmingoia.com>
2024-10-21 09:19:21 -0700

    Terraformer v2.8.1

--- Formula/terraformer-rootly.rb
+++ Formula/terraformer-rootly.rb
@@ -6,7 +6,7 @@ class TerraformerRootly < Formula
   on_macos do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/rootlyhq/terraformer/releases/download/v2.8.0/terraformer-rootly-darwin-arm64"
-      sha256 "1e20128ec1bf7aaf2be9310c76c91396a7167f6d6a4393cc6b4b605e0bc4f9fe"
+      sha256 "cebf26cf69a8bbc8dfd2e248ca839f1b76825b9b7460caea28131e07d3b14466"
 
       def install
         bin.install "terraformer-rootly-darwin-arm64" => "terraformer-rootly"
@@ -14,7 +14,7 @@ class TerraformerRootly < Formula
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/rootlyhq/terraformer/releases/download/v2.8.0/terraformer-rootly-darwin-amd64"
-      sha256 "53598559e2140eb002be89ebdf738634296c3d553926e65950c51765ccae613f"
+      sha256 "0eede4a260717716f879eddaa150d2eb2142ed1abf8546c2946f2615503e8479"
 
       def install
         bin.install "terraformer-rootly-darwin-amd64" => "terraformer-rootly"
@@ -25,7 +25,7 @@ class TerraformerRootly < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/rootlyhq/terraformer/releases/download/v2.8.0/terraformer-rootly-linux-arm64"
-      sha256 "b90f39563b71d3fd08ce58f4b259acbc0d09cf1634e5c47b21ce135e3c6ca352"
+      sha256 "8207fac47152ea59262019120a0e80a897ace6a6fcfc5c42ce527fb8b084e7dc"
 
       def install
         bin.install "terraformer-rootly-linux-arm64" => "terraformer-rootly"
@@ -33,7 +33,7 @@ class TerraformerRootly < Formula
     end
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/rootlyhq/terraformer/releases/download/v2.7.1/terraformer-rootly-linux-amd64"
-      sha256 "cd51da18775febe38468a36ec73fd0c84c9fdbe324d8db2ed769feaf18540316"
+      sha256 "253aad2e9b8f5583f6188b130717afd87ff593d2bf4924f62d0e5f0a8aed6999"
 
       def install
         bin.install "terraformer-rootly-linux-amd64" => "terraformer-rootly"
